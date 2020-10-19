d8b40fbbc46896c76ac5d32c361d3292b5a5b49a
jtsarvida <jtsarvida@gmail.com>
2020-10-19 12:54:29 -0400

    Started up a employeeDaoImplTest file

--- src/com/cognixia/jump/javafinalproject/test/employeeDaoImplTest.java
+++ src/com/cognixia/jump/javafinalproject/test/employeeDaoImplTest.java
@@ -1,4 +1,16 @@
-public class EmployeeDAOTest {
+package com.cognixia.jump.javafinalproject.test;
 
-    private 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class EmployeeDAOTest {
+    private testEmployees list = new getAllEmployees;
+
+    Address testAddress = new Address(123, "Blah Street", "Apartment 4", "Baltimore", "MD", "United States", "11111");
+    Employee bob = new Employee(8, 9, "Bob", "Saget", 22, "Comedian", 40000, "bsaget@gmail.com", "222-333-4444", testAddress);
+    
+    
 }
